47c690e60f4e5cb7e3744013f31badc11207774f
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-15 12:16:57 -0400

    add drink js display drinks

--- utils/displayDrinks.js
+++ utils/displayDrinks.js
@@ -1,8 +1,20 @@
 import fetchDrinks from './fetchDrinks.js';
+import get from './getElement.js';
 
 const displayDrinks = async (url) => {
   const data = await fetchDrinks(url);
-  console.log(data);
+  const drinks = data.drinks;
+  const drinksContainer = get('.drinks-center');
+  drinksContainer.innerHTML = drinks
+    .map((drink) => {
+      const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
+      return `<a href="singleDrink.html" class="single-drink" data-id="${id}">
+                <img src="${image}"
+                    alt="${name}" class="drink-image">
+                <span class="drink-name">${name}</span>
+            </a>`;
+    })
+    .join('');
 };
 
 export default displayDrinks;
