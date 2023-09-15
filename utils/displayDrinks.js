7f307c4d9c7279f83878ea1399596ae17b87ae9e
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-15 13:58:14 -0400

    add drink js loading feature

--- utils/displayDrinks.js
+++ utils/displayDrinks.js
@@ -1,20 +1,30 @@
 import fetchDrinks from './fetchDrinks.js';
 import get from './getElement.js';
+import { showLoading } from './loading.js';
+import { hideLoading } from './loading.js';
+
+const drinksContainer = get('.drinks-center');
 
 const displayDrinks = async (url) => {
-  const data = await fetchDrinks(url);
-  const drinks = data.drinks;
-  const drinksContainer = get('.drinks-center');
-  drinksContainer.innerHTML = drinks
-    .map((drink) => {
-      const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
-      return `<a href="singleDrink.html" class="single-drink" data-id="${id}">
+  drinksContainer.innerHTML = '';
+  showLoading;
+  try {
+    const data = await fetchDrinks(url);
+    const drinks = data.drinks;
+    drinksContainer.innerHTML = drinks
+      .map((drink) => {
+        const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
+        return `<a href="singleDrink.html" class="single-drink" data-id="${id}">
                 <img src="${image}"
                     alt="${name}" class="drink-image">
                 <span class="drink-name">${name}</span>
             </a>`;
-    })
-    .join('');
+      })
+      .join('');
+    hideLoading();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default displayDrinks;
